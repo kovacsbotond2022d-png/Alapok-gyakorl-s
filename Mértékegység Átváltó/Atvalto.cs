5f56f7aee8112bbbe4d0df0505f32b1c085351da
kovacsbotond2022d-png <kovacs.botond.2022@telekimezotur.hu>
2026-09-14 10:47:07 +0200

    2.feladat

---
+++ Mértékegység Átváltó/Atvalto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mértékegység_Átváltó
+{
+    internal class Atvalto
+    {
+        
+        public double Szorzo;
+
+        
+        public static double KmBolMeter(double km)
+        {
+            return km * 1000;
+        }
+
+        
+        public double EgyediAtvaltas(double ertek)
+        {
+            return ertek * Szorzo;
+        }
+    }
+}
---
+++ Mértékegység Átváltó/Program.cs
@@ -0,0 +1,30 @@
+﻿using Mértékegység_Átváltó;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mertekegyseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double meter = Atvalto.KmBolMeter(5);
+            Console.WriteLine($"5 km = {meter} m");
+
+            
+            Atvalto atvalto = new Atvalto();
+
+            
+            atvalto.Szorzo = 2.5;
+
+            
+            double eredmeny = atvalto.EgyediAtvaltas(10);
+            Console.WriteLine($"10 * {atvalto.Szorzo} = {eredmeny}");
+
+        }
+    }
+}
