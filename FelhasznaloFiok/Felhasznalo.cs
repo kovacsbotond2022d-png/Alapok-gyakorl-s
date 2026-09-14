69e4b17adaa12b84d02f879737a58f3b1c8cf5d4
kovacsbotond2022d-png <kovacs.botond.2022@telekimezotur.hu>
2026-09-14 11:13:37 +0200

    3.feladat

---
+++ FelhasznaloFiok/Felhasznalo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FelhasznaloFiok
+{
+    internal class Felhasznalo
+    {
+        private string jelszo = "123456";
+
+        public string Felhasznalonev { get; set; }
+
+        public void JelszoModositas(string regiJelszo, string ujJelszo)
+        {
+            if (regiJelszo == jelszo && ujJelszo.Length >= 6)
+            {
+                jelszo = ujJelszo;
+                Console.WriteLine("A jelszó sikeresen módosítva.");
+            }
+            else
+            {
+                Console.WriteLine("A jelszó módosítása sikertelen.");
+            }
+        }
+    }
+}
---
+++ FelhasznaloFiok/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FelhasznaloFiok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Felhasznalo felhasznalo = new Felhasznalo();
+
+                felhasznalo.Felhasznalonev = "botond";
+
+                Console.WriteLine($"Felhasználónév: {felhasznalo.Felhasznalonev}");
+
+                Console.WriteLine("\nHibás régi jelszó:");
+                felhasznalo.JelszoModositas("rosszjelszo", "ujjelszo");
+
+                Console.WriteLine("\nTúl rövid új jelszó:");
+                felhasznalo.JelszoModositas("123456", "123");
+
+                Console.WriteLine("\nHelyes módosítás:");
+                felhasznalo.JelszoModositas("123456", "titkos123");
+            
+        }
+    }
+}
