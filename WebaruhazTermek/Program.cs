442c86a70b3e288178ace6b5a653dd7758d1b716
kovacsbotond2022d-png <kovacs.botond.2022@telekimezotur.hu>
2026-09-14 10:26:42 +0200

    alapok gyakorlasa

---
+++ WebaruhazTermek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebaruhazTermek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Termek termek1 = new Termek();
+                termek1.Nev = "monitor";
+                termek1.Egysegar = 350000;
+                termek1.Keszlet = 15;
+
+                Termek termek2 = new Termek();
+                termek2.Nev = "bilentyuzet";
+                termek2.Egysegar = -500;   
+                termek2.Keszlet = -10;     
+
+                Console.WriteLine("1. termék:");
+                Console.WriteLine($"Név: {termek1.Nev}");
+                Console.WriteLine($"Egységár: {termek1.Egysegar} Ft");
+                Console.WriteLine($"Készlet: {termek1.Keszlet} db");
+
+                Console.WriteLine();
+
+                Console.WriteLine("2. termék:");
+                Console.WriteLine($"Név: {termek2.Nev}");
+                Console.WriteLine($"Egységár: {termek2.Egysegar} Ft");
+                Console.WriteLine($"Készlet: {termek2.Keszlet} db");
+            
+        }
+    }
+}
---
+++ WebaruhazTermek/Termek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebaruhazTermek
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+
+        private int egysegar;
+        public int Egysegar
+        {
+            get { return egysegar; }
+            set
+            {
+                if (value < 1)
+                    egysegar = 1;
+                else
+                    egysegar = value;
+            }
+        }
+
+        private int keszlet;
+        public int Keszlet
+        {
+            get { return keszlet; }
+            set
+            {
+                if (value < 0)
+                    keszlet = 0;
+                else
+                    keszlet = value;
+            }
+        }
+    }
+}
